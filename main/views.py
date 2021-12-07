b684f3a3d4e74bb9d1c630f41334b0b02d3a188a
hubbub-inc <matthewcarrella9@gmail.com>
2021-12-06 21:39:08 -0500

    update calendar main page

--- main/views.py
+++ main/views.py
@@ -4,6 +4,9 @@ from django.http import HttpResponse
 from django.views import View
 from .models import TestModel
 from django.views.generic.detail import DetailView
+from datetime import date
+import datetime
+from cal.models import Event
 
 
 def home(request):
@@ -14,12 +17,28 @@ def home(request):
 
 def about(request):
     context = {}
+    print("about")
+    today = datetime.datetime.today().date()
+
+
+    addweek = today + datetime.timedelta(days=20)
+
+
+    print(type(today))
+    print(type(addweek))
+    upcoming = Event.objects.filter(start_time__range=(today, addweek))
+    print(upcoming)
+
+    print(addweek)
     template = 'home/about.html'
+    context['upcoming'] = [{'title': event.title, 'day': str(event.start_time.day), 'month': event.start_time.strftime("%B")} for event in upcoming]
     return render(request, template, context)
 
 
 def contact(request):
     context = {}
+
+
     template = 'home/contact.html'
     return render(request, template, context)
 
